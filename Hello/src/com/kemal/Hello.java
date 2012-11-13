7b8f787a4d7551b2a0d648747549079e5dc033fd
Przemek <przemek@natan.pl>
2012-11-13 23:05:08 +0100

    initial1

---
+++ Hello/src/com/kemal/Hello.java
@@ -0,0 +1,14 @@
+package com.kemal;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+	}
+
+}
